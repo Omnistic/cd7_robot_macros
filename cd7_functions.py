0ab6dcf6dca4dfc4e1e85addde3a6b796319427d
David Nguyen <64271861+Omnistic@users.noreply.github.com>
2024-06-06 11:09:38 +0200

    Added file for magnification change
    Moved all parameters of the main script at the top. Still confused why we need that many experiments.

---
+++ cd7_functions.py
@@ -0,0 +1,52 @@
+﻿# Note: I'm not sure if immersion is applied when activating
+# the 50x objective in this manner (to be tested).
+
+OBJECTIVES = {
+    '5x0.35NA': 2,
+    '20x0.7NA': 1,
+    '20x0.95NA': 3,
+    '50x1.2NA': 4
+    }
+
+OPTOVARS = {
+    '0.5x': 3,
+    '1x': 2,
+    '2x': 1
+    }
+
+def set_magnification(Zen, objective, optovar='1x'):
+    ###
+    #
+    # Set CD7 overall magnification, i.e. objective
+    # and optovar magnifications.
+    #
+    # Parameters
+    # ==========
+    # Zen: Zeiss.Micro.LM.Scripting.Research.ZenBlueScriptingWrapperServiceLM
+    #   An object from the main script that probably represents the CD7.
+    #
+    # objective: string
+    #   The objective magnification.
+    #
+    # optovar: string
+    #   The optovar magnification. Default to 1x.
+    #
+    # Returns
+    # =======
+    # boolean
+    #   True if the magnification could be set. False otherwise.
+    #
+    ###
+
+    try:
+        if Zen.Devices.ObjectiveChanger.TargetPosition != OBJECTIVES[objective]:
+            Zen.Devices.ObjectiveChanger.TargetPosition = OBJECTIVES[objective]
+            Zen.Devices.ObjectiveChanger.Apply()
+
+        if Zen.Devices.Optovar.TargetPosition != OPTOVARS[optovar]:
+            Zen.Devices.Optovar.TargetPosition = OPTOVARS[optovar]
+            Zen.Devices.Optovar.Apply()
+
+        return True
+    except:
+        return False
--- sample_functions.py
+++ sample_functions.py
@@ -8,34 +8,46 @@ BARCODE_DUMMY_LENGTH = 9
 def get_barcode(zen_image):
     ###
     #
-    # Returns formatted barcode from the ZenImage metadata
+    # Get and format barcode from the ZenImage metadata.
     #
     # Parameters
     # ==========
     # zen_image: Zeiss.Micro.Scripting.ZenImage
+    #   A ZenImage acquired with the CD7.
     #
-    # Return
-    # ======
+    # Returns
+    # =======
     # barcode: string
+    #   The formatted barcode.
     #
     ###
-    
-    # Get barcode text from metadata element
-    # Note: returns an empty string if barcode element missing from metadata
+
+    # Get barcode text from metadata element.
+    # Note: returns an empty string if barcode element missing from metadata.
     try:
         barcode_text = zen_image.Metadata.GetMetadataStringWithPath(BARCODE_XML_PATH)
     except:
         barcode_text = ''
-    
-    # Extract barcode string from element text (e.g. "left barcode: the_barcode")
-    # Note: doesn't work if there are spaces within the barcode
+
+    # Extract barcode string from element text (e.g. "left barcode: the_barcode").
+    # Note: doesn't work if there are spaces within the barcode.
     barcode = barcode_text.split(' ')[-1]
-    
-    # If barcode is missing (empty string), replace it with a dummy string
+
+    # If barcode is missing (empty string), replace it with a dummy string.
     if barcode == '':
         barcode = ''.join(random.choice(BARCODE_DUMMY_CHARS) for _ in range(BARCODE_DUMMY_LENGTH))
-    
+
     return barcode
 
 def get_timestamp():
+    ###
+    #
+    # Get a timestamp in a YYYYMMDDHHMM format.
+    #
+    # Returns
+    # =======
+    # string
+    #   The formatted timestamp.
+    #
+    ###
     return time.strftime("%Y%m%d%H%M")
