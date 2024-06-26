10a6a328df8b64a5c9b23ec558ed27b97ec317c5
David Nguyen <64271861+Omnistic@users.noreply.github.com>
2024-06-26 16:35:00 +0200

    Add files via upload

--- _cd7_functions.py
+++ _cd7_functions.py
@@ -1,4 +1,7 @@
-﻿# Note: I'm not sure if immersion is applied when activating
+﻿import os
+import xml.etree.ElementTree as ET
+
+# Note: I'm not sure if immersion is applied when activating
 # the 50x objective in this manner (to be tested).
 
 OBJECTIVES = {
@@ -50,3 +53,51 @@ def set_magnification(Zen, objective, optovar='1x'):
         return True
     except:
         return False
+
+def get_experiment_folder(Zen):
+    active_experiment = Zen.Acquisition.Experiments.ActiveExperiment.FileName
+    
+    return active_experiment[:active_experiment.rfind('\\')+1]
+
+class TruncatedExperiment:
+    def __init__(self, Zen, experiment_template):
+        self.Zen = Zen
+        self.experiment_template = experiment_template
+        self.get_experiment_folder()
+        self.truncated_experiment_file = tempfile.NamedTemporaryFile(mode='w+t', prefix=experiment_template+'_truncated', suffix='.czexp', dir=self.experiment_folder, delete=False)
+        self.truncated_experiment = self.truncated_experiment_file.name.split('\\')[-1]
+        self.get_experiment_root(experiment_template)
+        self.get_tile_region_arrays()
+        self.truncate_experiment()
+
+    def get_experiment_folder(self):
+        active_experiment = self.Zen.Acquisition.Experiments.ActiveExperiment.FileName
+        self.experiment_folder = active_experiment[:active_experiment.rfind('\\')+1]
+
+    def get_experiment_root(self, experiment_template):
+        self.experiment_tree = ET.parse(self.experiment_folder + self.experiment_template + '.czexp')
+        self.experiment_root = self.experiment_tree.getroot()
+
+    def get_tile_region_arrays(self):
+        self.tile_region_arrays = self.experiment_root.find('./ExperimentBlocks/AcquisitionBlock/SubDimensionSetups/RegionsSetup/SampleHolder/SingleTileRegionArrays')
+
+    def truncate_experiment(self):
+        ii = -1
+        for tile_region_array in self.tile_region_arrays:
+            ii += 1
+            if ii < 1:
+                tile_region_array.find('./IsUsedForAcquisition').text = 'true'
+                continue
+            tile_region_array.find('./IsUsedForAcquisition').text = 'false'
+
+        self.experiment_tree.write(self.truncated_experiment_file.name)
+        self.truncated_experiment_file.close()
+
+    def close(self):
+        try:
+            self.truncated_experiment_file.close()
+        except:
+            pass
+
+        if os.path.exists(self.truncated_experiment_file.name):
+            os.remove(self.truncated_experiment_file.name)
---
+++ _logging.py
@@ -0,0 +1,6 @@
+﻿from datetime import datetime
+
+def log_message(path, verbose, message):
+    if verbose:
+        with open(path, 'a') as log_file:
+            log_file.write('[{0}] {1}\n'.format(datetime.now(), message))
